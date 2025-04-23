9aa1ac92a4c22738417457064dea4eb979bbf653
Angel Juarez <angelnjuarez@gmail.com>
2025-04-23 11:53:41 -0300

    practica 1 v4

--- src/clase12practica1/Agenda.java
+++ src/clase12practica1/Agenda.java
@@ -42,23 +42,52 @@ public class Agenda {
 		this.contactos = nuevoContacto;
 		this.telefonos = nuevoTelefonos;
 	}
-	
+
 	void eliminar(Persona contacto) {
 		for (int i = 0; i < this.contactos.length; i++) {
-			if(this.contactos[i].mismaPersona(contacto)) {
+			if (this.contactos[i].mismaPersona(contacto)) {
 				this.contactos[i] = null;
 				this.telefonos[i] = null;
 				return;
 			}
 		}
 	}
+
+	boolean pertenece(Persona contacto) {
+		for (int i = 0; i < this.contactos.length; i++) {
+			if (this.contactos[i].mismaPersona(contacto)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	String dameTelefono(Persona contacto) {
+		if (this.pertenece(contacto)) {
+			for (int i = 0; i < this.contactos.length; i++) {
+				if (this.contactos[i].mismaPersona(contacto)) {
+					return this.telefonos[i];
+				}
+			}
+		}
+		throw new RuntimeException("El contacto no está");
+	}
 	
+	int perteneceV2(Persona contacto) {
+		for (int i = 0; i < this.contactos.length; i++) {
+			if (this.contactos[i].mismaPersona(contacto)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 	
-	
-	
-	
-	
-	
-	
+	String dameTelefonoV2(Persona contacto) {
+		int pos = perteneceV2(contacto);
+		if (pos != -1) {
+			return this.telefonos[pos];
+		}
+		throw new RuntimeException("El contacto no está");
+	}
 
 }
--- src/clase12practica1/Punto.java
+++ src/clase12practica1/Punto.java
@@ -3,4 +3,14 @@ package clase12practica1;
 public class Punto {
 	double x;
 	double y;
+
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	static double distancia(Punto p1, Punto p2) {
+		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+	}
+
 }
